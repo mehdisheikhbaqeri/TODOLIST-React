73af24c255d454664fe1a06f5b2b165e93155921
Mehdi <mehdi7baqeri@gmail.com>
2022-08-19 00:47:11 +0430

    start modal and added close button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PageTitle from "./Component/PageTitle";
 import AppHeader from "./Component/AppHeader";
+import AppContent from "./Component/AppContent";
 import Styles from "./Styles/modules/app.module.scss";
 // import { SelectButton } from "./Component/Button";
 
@@ -9,7 +10,8 @@ function App() {
     <div className="container">
       <PageTitle>TODO LIST</PageTitle>
       <div className={Styles.app__wrapper}></div>
-      <AppHeader/>
+      <AppHeader />
+      <AppContent />
     </div>
   );
 }
---
+++ src/Component/AppContent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AppContent() {
+  return <div>This Is App Content</div>;
+}
+
+export default AppContent;
