5fb3892fc69af0348f009f75c77b5bb93555a4fa
Mehdi <mehdi7baqeri@gmail.com>
2022-09-04 03:43:08 +0430

    end filter section

--- src/Component/AppHeader.js
+++ src/Component/AppHeader.js
@@ -3,15 +3,24 @@ import React, { useState } from "react";
 import Button, { SelectButton } from "./Button";
 import styles from "../Styles/modules/app.module.scss";
 import TodoModal from "./TodoModal";
+import { useDispatch, useSelector } from "react-redux";
+import { updateFilterStatus } from "../slices/todoSlice";
 
 function AppHeader() {
   const [modalOpen, setModalOpen] = useState(false);
+  const filterStatus = useSelector((state) => state.todo.filterStatus);
+
+  const dispatch = useDispatch();
+
+  const updateFilter = (e) => {
+    dispatch(updateFilterStatus(e.target.value));
+  };
   return (
     <div className={styles.appHeader}>
       <Button variant="primary" onClick={() => setModalOpen(true)}>
         Add Tasks
       </Button>
-      <SelectButton id="status">
+      <SelectButton id="status" value={filterStatus} onChange={updateFilter}>
         <option value="All">ALL</option>
         <option value="Incomplete">Incomplete</option>
         <option value="Complete">Complete</option>
